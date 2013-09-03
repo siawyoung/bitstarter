94934241ff3e44557c0d7d5bc3d9fe33892fb3a5
Ubuntu <ubuntu@ip-172-31-13-50.ap-southeast-1.compute.internal>
2013-09-03 10:32:14 +0000

    Tidied up navbar

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://mighty-hamlet-3410.herokuapp.com/";
+var rest = require('restler');
+var sys = require('util');
 
 var assertFileExists = function(infile){
 	var instr = infile.toString();
@@ -34,6 +37,10 @@ var assertFileExists = function(infile){
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
 	}
 	return instr;
+};
+
+var assertUrlExists = function(url){
+	return url.toString();
 };
 
 var cheerioHtmlFile = function(htmlfile){
@@ -55,6 +62,17 @@ var checkHtmlFile = function (htmlfile, checksfile){
 	return out;
 };
 
+var checkURL = function (htmlfile, checksfile){
+        $ = cheerio.load(htmlfile);
+        var checks = loadChecks(checksfile).sort();
+        var out = {};
+        for (var ii in checks){
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+        }
+        return out;
+};
+
 var clone = function(fn){
 	// Workaround for commander.js issue.
 	// http://stackoverflow.com/a/6772648
@@ -65,8 +83,16 @@ if(require.main == module){
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL to web page', clone(assertUrlExists), URL_DEFAULT)
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
+	if(program.url) {
+	rest.get(program.url).on('complete', function(result){
+	var checkJson = checkURL(result, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);});
+}
+	else{
+	var checkJson = checkHtmlFile(program.file, program.checks)};
 	var outJson = JSON.stringify(checkJson, null, 4);
 	console.log(outJson);
 }
